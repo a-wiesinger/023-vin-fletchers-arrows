9ea21bf428e7da2b8ab28db5c8ee9cf7e12e2294
Aaron Wiesinger <aaron.wiesinger@gmail.com>
2022-09-20 21:48:34 -0400

    update verbiage

--- Program.cs
+++ Program.cs
@@ -2,17 +2,15 @@
  * Vin Fletcher's Arrows
  * :: Classes ::
  */
- 
- // 
- 
- using _023_vin_fletchers_arrows.Models;
- 
- // TODO : User selects arrowhead type
- // TODO : User selects fletching
- // TODO : User selects length of shaft
- 
- // TODO : Earlier selections create instance of Arrow class
- 
- // TODO : Create float GetCost(){} method to do just what you think :P 
- 
- // TODO : Display the calculated arrow cost to the user
+
+using _023_vin_fletchers_arrows.Models;
+
+// TODO : User selects arrowhead type
+// TODO : User selects fletching
+// TODO : User selects length of shaft
+
+// TODO : Earlier selections create instance of Arrow class
+
+// TODO : Create float GetCost(){} method to do just what you think :P 
+
+// TODO : Display the calculated arrow cost to the user
