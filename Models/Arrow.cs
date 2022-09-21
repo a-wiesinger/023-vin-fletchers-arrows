3e1b26e2db58c6442e264aada77c397fb86ee4f7
Aaron Wiesinger <aaron.wiesinger@gmail.com>
2022-09-20 21:36:24 -0400

    project setup

---
+++ Models/Arrow.cs
@@ -0,0 +1,19 @@
+namespace _023_vin_fletchers_arrows.Models
+{
+    public class Arrow
+    {
+        public enum Arrowhead
+        {
+            Steel,
+            Wood,
+            Obsidian
+        }
+        public enum Fletching
+        {
+            Plastic,
+            TurkeyFeathers,
+            GooseFeathers
+        }
+        public float _length;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,4 +5,14 @@
  
  // 
  
+ using _023_vin_fletchers_arrows.Models;
  
+ // TODO : User selects arrowhead type
+ // TODO : User selects fletching
+ // TODO : User selects length of shaft
+ 
+ // TODO : Earlier selections create instance of Arrow class
+ 
+ // TODO : Create float GetCost(){} method to do just what you think :P 
+ 
+ // TODO : Display the calculated arrow cost to the user
