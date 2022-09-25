53853d49452b705a7656a5e5bccc56616fa559c7
Aaron Wiesinger <aaron.wiesinger@gmail.com>
2022-09-25 07:45:12 -0400

    Refactor arrow class

--- Models/Arrow.cs
+++ Models/Arrow.cs
@@ -2,19 +2,15 @@ namespace _023_vin_fletchers_arrows.Models
 {
     public class Arrow
     {
-        // private readonly Arrowhead _aHead;
-        // private readonly Fletching _aFletching;
-        // private float _sLength;
-
         public Arrowhead ArrowheadCore { get; set; }
         public Fletching FletchingCore { get; set; }
         public float SLengthCore { get; set; }
 
-        public Arrow(Arrowhead aHead, Fletching aFletching, float sLength)
+        public Arrow()
         {
-            ArrowheadCore = aHead;
-            FletchingCore = aFletching;
-            SLengthCore = sLength;
+            ArrowheadCore = ArrowheadSelect();
+            FletchingCore = FletchingSelect();
+            SLengthCore = ShaftLengthSelect();
         }
         
         public enum Arrowhead
@@ -32,11 +28,90 @@ namespace _023_vin_fletchers_arrows.Models
             TurkeyFeathers = 5,
             GooseFeathers = 3
         }
+        
+        // Arrowhead selection
+        private Arrowhead ArrowheadSelect()
+        {
+            Console.WriteLine("Please select your arrowhead type: ");
+            Console.WriteLine("Options: wood, steel, obsidian");
+            string? userInputArrowhead = Console.ReadLine();
 
-        public float GetCost(Arrowhead aHead, Fletching aFletching, float sLength)
+            if (userInputArrowhead == null) ArrowheadSelect();
+            if (userInputArrowhead == Arrowhead.Steel.ToString().ToLower())
+            {
+                Console.WriteLine("Steel arrowhead, got it!");
+                return Arrowhead.Steel;
+            }
+            if (userInputArrowhead == Arrowhead.Wood.ToString().ToLower())
+            {
+                Console.WriteLine("Wood, eh? Your funeral.");
+                return Arrowhead.Wood;
+            }
+            if (userInputArrowhead == Arrowhead.Obsidian.ToString().ToLower())
+            {
+                Console.WriteLine("Oooh, keepin' it old school.");
+                return Arrowhead.Obsidian;
+            }
+            Console.Write("Please enter a valid selection. Press a key to try again. ");
+            Console.ReadLine();
+            ArrowheadSelect();
+            
+            // Req return statement
+            return Arrowhead.Steel;
+        }
+
+        // Fletching selection
+        private Fletching FletchingSelect() 
+        {
+            Console.WriteLine("Please select your fletching (feathery stuff at the end): ");
+            Console.WriteLine("Options: goosefeathers, turkeyfeathers, plastic");
+            string? userInputFletching = Console.ReadLine();
+
+            if (userInputFletching == null) FletchingSelect();
+            if (userInputFletching == Fletching.Plastic.ToString().ToLower())
+            {
+                Console.WriteLine("Plastic, eh? Nice.");
+                return Fletching.Plastic;
+            }
+            if (userInputFletching == Fletching.TurkeyFeathers.ToString().ToLower())
+            {
+                Console.WriteLine("Gobble gobble!");
+                return Fletching.TurkeyFeathers;
+            }
+            if (userInputFletching == Fletching.GooseFeathers.ToString().ToLower())
+            {
+                Console.WriteLine("Mmmmm so soft.");
+                return Fletching.GooseFeathers;
+            }
+            Console.Write("Please enter a valid selection. Press a key to try again. ");
+            Console.ReadLine();
+            FletchingSelect();
+            
+            // Catch-all return
+            return Fletching.Plastic;
+        }
+
+        // Shaft length selection
+        private float ShaftLengthSelect()
+        {
+            // Get user input
+            Console.Write("How long do you need the arrow shaft to be? (In centimeters, please!) ");
+            string? userInputShaftLength = Console.ReadLine();
+            
+            // Bad state checks
+            if (userInputShaftLength == null) ShaftLengthSelect();
+            
+            // Convert to float and check again
+            float shaftLengthNum = Convert.ToSingle(userInputShaftLength);
+            if (shaftLengthNum < 0 || shaftLengthNum > 100) ShaftLengthSelect();
+
+            return (shaftLengthNum > 0) ? shaftLengthNum : ShaftLengthSelect();
+        }
+
+        public float GetCost()
         {
             float costPerCentimeter = 0.05f;
-            float total = (int)aHead + (int)aFletching + (costPerCentimeter * sLength);
+            float total = (int)ArrowheadCore + (int)FletchingCore + (costPerCentimeter * SLengthCore);
 
             return total;
         }
